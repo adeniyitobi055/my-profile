6200f62d36e60ab7da83ecf7c917b0e3b79f565b
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2024-01-28 09:13:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ window.onscroll = () => {
         activeLink.classList.add("active");
       }
     } else {
-      sec.classList.remove("show-animate");
+      // sec.classList.remove("show-animate");
     }
   });
 };
@@ -44,39 +44,3 @@ document.addEventListener("keydown", function (event) {
 });
 
 document.removeEventListener("keydown", Event);
-
-// // Handle section animation
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   // Function to check if an element is in viewport
-//   function isInViewport(element) {
-//     const rect = element.getBoundingClientRect();
-//     return (
-//       rect.top >= 0 &&
-//       rect.left >= 0 &&
-//       rect.bottom <=
-//         (window.innerHeight || document.documentElement).clientHeight &&
-//       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//     );
-//   }
-
-//   // Function to handle scroll event
-//   function handleScroll() {
-//     const animatedSections = document.querySelectorAll(
-//       ".hero-section, .about-section"
-//     );
-
-//     animatedSections.forEach((section) => {
-//       if (isInViewport(section)) {
-//         section.style.opacity = 1;
-//         section.style.transform = "translateY(0)";
-//       }
-//     });
-//   }
-
-//   // Add event lister for scroll event
-//   window.addEventListener("scroll", handleScroll);
-
-//   // Initial check on page load
-//   handleScroll();
-// });
