171b444788be6fde244c81460c6f13a7f025dbbf
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2024-01-31 14:58:27 +0100

    update project

--- script.js
+++ script.js
@@ -25,22 +25,64 @@ window.onscroll = () => {
   });
 };
 
-// Handle click for certificate images
-function toggleImage(element) {
-  const certificateContainer = element;
-  certificateContainer.classList.toggle("clicked");
+// // Handle click for certificate images
+// function toggleImage(element) {
+//   const certificateContainer = element;
+//   certificateContainer.classList.toggle("clicked");
+// }
+
+// // Add event for escape key press on certificate images
+// document.addEventListener("keydown", function (event) {
+//   if (event.key === "Escape") {
+//     const certificateContainer = document.querySelectorAll(
+//       ".certificate-container.clicked"
+//     );
+//     certificateContainer.forEach(function (container) {
+//       container.classList.remove("clicked");
+//     });
+//   }
+// });
+
+// Function to close and open active tabs or links
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
-// Add event for escape key press on certificate images
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
-    const certificateContainer = document.querySelectorAll(
-      ".certificate-container.clicked"
-    );
-    certificateContainer.forEach(function (container) {
-      container.classList.remove("clicked");
-    });
-  }
-});
+let sidemenu = document.getElementById("sidemenu");
 
-document.removeEventListener("keydown", Event);
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyyW0H29JxPiN43AmYzJpNTT9XFCgwAJcIE3t1kAYMQiXXHN5yHRbFMv_y8IHuebqcv/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  msg.innerHTML = "Message sending...";
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
